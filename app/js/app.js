4290e663c28832a4189fbca60ea62ce356622d7b
Yuriy Tarnavskiy <yuriy.tarnavskiy@ekreative.com>
2015-01-21 18:22:53 +0200

    added menu, ui-router, 2 template

--- app/js/app.js
+++ app/js/app.js
@@ -2,4 +2,22 @@
 
 /* App Module */
 
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ui.router', 'myAppControllers']);
+
+myApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+    $urlRouterProvider.otherwise("/");
+    $stateProvider
+        .state('home', {
+            url: "/",
+            templateUrl: "views/home.html",
+            controller: 'aboutCtrl'
+        })
+        .state('about', {
+            url: "/about",
+            templateUrl: "views/about.html",
+            controller: "aboutCtrl"
+        });
+
+    $locationProvider.html5Mode(true)
+});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,3 +3,14 @@
 /* Controllers */
 
 var myAppControllers = angular.module('myAppControllers', []);
+
+myAppControllers.controller('aboutCtrl', ['$scope',
+        function($scope){
+            $scope.about = $state.current.name;
+        }
+    ]);
+
+myAppControllers.controller('homeCtrl', ['$scope', '$state',
+    function($scope){ }
+]);
+
